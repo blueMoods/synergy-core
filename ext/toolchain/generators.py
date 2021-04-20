5778cf014a64e42040ac17a1d24728a0eaf3f9b2
Geng Yuchao <gengyuchao11@163.com>
2021-04-20 17:26:18 +0800

    Change toolchain python version from  2to3.

--- ext/toolchain/generators.py
+++ ext/toolchain/generators.py
@@ -15,70 +15,70 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 class Generator(object):
-	def __init__(self, cmakeName, buildDir='build', sourceDir='..', binDir='bin'):
-		self.cmakeName = cmakeName
-        	self.buildDir = buildDir
-		self.sourceDir = sourceDir
-		self.binDir = binDir
+    def __init__(self, cmakeName, buildDir='build', sourceDir='..', binDir='bin'):
+        self.cmakeName = cmakeName
+        self.buildDir = buildDir
+        self.sourceDir = sourceDir
+        self.binDir = binDir
 
-	def getBuildDir(self, target):
-		return self.buildDir
+    def getBuildDir(self, target):
+        return self.buildDir
 
-	def getBinDir(self, target=''):
-		return self.binDir
+    def getBinDir(self, target=''):
+        return self.binDir
 
-	def getSourceDir(self):
-		return self.sourceDir
+    def getSourceDir(self):
+        return self.sourceDir
 
 class VisualStudioGenerator(Generator):
-	def __init__(self, version):
-		super(VisualStudioGenerator, self).__init__('Visual Studio ' + version)
+    def __init__(self, version):
+        super(VisualStudioGenerator, self).__init__('Visual Studio ' + version)
 
-	def getBinDir(self, target=''):
-		return super(VisualStudioGenerator, self).getBinDir(target) + '/' + target
+    def getBinDir(self, target=''):
+        return super(VisualStudioGenerator, self).getBinDir(target) + '/' + target
 
 class MakefilesGenerator(Generator):
-	def __init__(self):
-		super(MakefilesGenerator, self).__init__('Unix Makefiles')
+    def __init__(self):
+        super(MakefilesGenerator, self).__init__('Unix Makefiles')
 
-	def getBuildDir(self, target):
-		return super(MakefilesGenerator, self).getBuildDir(target) + '/' + target
+    def getBuildDir(self, target):
+        return super(MakefilesGenerator, self).getBuildDir(target) + '/' + target
 
-	def getBinDir(self, target=''):
-		workingDir = super(MakefilesGenerator, self).getBinDir(target)
-		
-		# only put debug files in separate bin dir
-		if target == 'debug':
-			workingDir += '/debug'
-		
-		return workingDir
+    def getBinDir(self, target=''):
+        workingDir = super(MakefilesGenerator, self).getBinDir(target)
+        
+        # only put debug files in separate bin dir
+        if target == 'debug':
+            workingDir += '/debug'
+        
+        return workingDir
 
-	def getSourceDir(self):
-		return super(MakefilesGenerator, self).getSourceDir() + '/..'
-		
+    def getSourceDir(self):
+        return super(MakefilesGenerator, self).getSourceDir() + '/..'
+        
 class XcodeGenerator(Generator):
-	def __init__(self):
-		super(XcodeGenerator, self).__init__('Xcode')
-		
-	def getBinDir(self, target=''):
-		if target == "":
-			return super(XcodeGenerator, self).getBinDir(target)
-		
-		xcodeTarget = target[0].upper() + target[1:]
-		return super(XcodeGenerator, self).getBinDir(target) + '/' + xcodeTarget
+    def __init__(self):
+        super(XcodeGenerator, self).__init__('Xcode')
+        
+    def getBinDir(self, target=''):
+        if target == "":
+            return super(XcodeGenerator, self).getBinDir(target)
+        
+        xcodeTarget = target[0].upper() + target[1:]
+        return super(XcodeGenerator, self).getBinDir(target) + '/' + xcodeTarget
 
 class EclipseGenerator(Generator):
-	def __init__(self):
-		super(EclipseGenerator, self).__init__('Eclipse CDT4 - Unix Makefiles', '', '')
+    def __init__(self):
+        super(EclipseGenerator, self).__init__('Eclipse CDT4 - Unix Makefiles', '', '')
 
-	def getBuildDir(self, target):
-		# eclipse only works with in-source build.
-		return ''
+    def getBuildDir(self, target):
+        # eclipse only works with in-source build.
+        return ''
 
-	def getBinDir(self, target=''):
-		# eclipse only works with in-source build.
-		return 'bin'
+    def getBinDir(self, target=''):
+        # eclipse only works with in-source build.
+        return 'bin'
 
-	def getSourceDir(self):
-		return ''
+    def getSourceDir(self):
+        return ''
 
